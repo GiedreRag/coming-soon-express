6397cb11a38abe708e55c7515f869002c89f9539
Giedre <giedre.rageliene@gmail.com>
2023-08-09 08:38:09 +0300

    app 404 and 500

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ app.get('/', (req, res) => {
     res.sendFile('index.html');
   });
 
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!")
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
